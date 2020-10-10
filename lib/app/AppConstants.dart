95e763b5eb3291da7f12ba644f5ce80de9c8584b
Jitendra Kumar <jitendrasoam90@gmail.com>
2020-10-10 10:43:27 +0530

    initial commit

--- lib/app/AppConstants.dart
+++ lib/app/AppConstants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
-  static const int user_id = 406;
-  static const int konnect_id = 1330;
+  static const int user_id = 5379;
+  static const int konnect_id = 5745;
 
   int get userId => user_id;
 
@@ -15,7 +15,7 @@ class AppConstants {
       'AAAAc5_l7dk:APA91bGq3HyBqzeyJZfSeS-zRWFQIs_cVjmM498VmE92_e3QPQT-Ox7-CH_8AT9345ye7Cbm3hpHix43R8griTqtdimnlpNw-zCF9IE4PB8CrCf5Bmu2QUzUSEs82WN0xhdSon-qym3-';
 
   static const String SHARE_APP = 'check out my app \n\n'
-      'https://play.google.com/store/apps/details?id=com.mrk.tradex';
+      'https://play.google.com/store/apps/details?id=com.app.krishnasupermarket';
 
   static const String SHARE_STORE = 'check out my online store \n\n'
       'https://meo.co.in/konnect-link/konnect-new/konnect-newlink.php?konnect_id=$konnect_id#product';
